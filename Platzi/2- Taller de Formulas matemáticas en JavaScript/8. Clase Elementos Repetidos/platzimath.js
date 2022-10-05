b5463f22364367a89ceda7ba9f25b554ba92a3d0
Ankush Bihal <ankush.bihal@triggle.app>
2022-10-05 13:09:55 +0200

    Clase Calcular la Moda (Elementos Repetidos)

--- Platzi/2- Taller de Formulas matemáticas en JavaScript/8. Clase Elementos Repetidos/platzimath.js
+++ Platzi/2- Taller de Formulas matemáticas en JavaScript/8. Clase Elementos Repetidos/platzimath.js
@@ -1,9 +1,30 @@
 function esPar(lista) {
   return !(lista.length % 2);
 }
-
 function esImpar(lista) {
   return lista.length % 2;
+}
+
+function calcularModa(lista) {
+  const listaCount = {};
+
+  for (let i = 0; i < lista.length; i++) {
+    const elemento = lista[i];
+
+    if (listaCount[elemento]) {
+      listaCount[elemento] += 1;
+    } else {
+      listaCount[elemento] = 1;
+    }
+  }
+
+  const listaArray = Object.entries(listaCount);
+  const listaOrdenada = ordenarListaBidimensional(listaArray, 1);
+  const listaMaxNumber = listaOrdenada[listaOrdenada.length - 1];
+  const moda = listaMaxNumber[0];
+  // console.log({listaCount, listaArray, listaOrdenada, listaMaxNumber});
+  // console.log('La moda es: ' + listaMaxNumber[0]);
+  return moda;
 }
 
 function calcularMediana(listaDesordenada) {
@@ -14,7 +35,6 @@ function calcularMediana(listaDesordenada) {
     const indexMitad1ListaPar = lista.length / 2 - 1;
     const indexMitad2ListaPar = lista.length / 2;
     const listaMitades = [];
-
     listaMitades.push(lista[indexMitad1ListaPar]);
     listaMitades.push(lista[indexMitad2ListaPar]);
 
@@ -23,10 +43,8 @@ function calcularMediana(listaDesordenada) {
   } else {
     const indexMitadListaImpar = Math.floor(lista.length / 2);
     const medianaListaImpar = lista[indexMitadListaImpar];
-
     console.log(indexMitadListaImpar);
     console.log(medianaListaImpar);
-
     return medianaListaImpar;
   }
 }
@@ -44,23 +62,21 @@ function calcularPromedio(lista) {
 
 function ordenarLista(listaDesordenada) {
   function ordenarListaSort(valorAcumulado, nuevoValor) {
-    // if (valorAcumulado > nuevoValor) {
-    //   return 1;
-    // } else if (valorAcumulado == nuevoValor) {
-    //   return 0;
-    // } else if (valorAcumulado < nuevoValor) {
-    //   return -1;
-    // }
-
     return valorAcumulado - nuevoValor;
-    // return nuevoValor - valorAcumulado;
-    // return 5 - 10 -> -5;
-    // return 5 - 5 -> 0;
-    // return 10 - 5 -> 5;
   }
 
-  // const lista = listaDesordenada.sort(ordenarListaSort);
-  const lista = listaDesordenada.sort((a, b) => a - b);
+  // const lista = listaDesordenada.sort((a,b) => a-b);
+  const lista = listaDesordenada.sort(ordenarListaSort);
 
+  return lista;
+}
+
+// [ [0,1],  [0,1],  [0,1] ]
+function ordenarListaBidimensional(listaDesordenada, i) {
+  function ordenarListaSort(valorAcumulado, nuevoValor) {
+    return valorAcumulado[i] - nuevoValor[i];
+  }
+
+  const lista = listaDesordenada.sort(ordenarListaSort);
   return lista;
 }
