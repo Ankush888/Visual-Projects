5644da6d7f6a1039287abc5a005e18d982347ba6
Ankush Bihal <ankush.bihal@triggle.app>
2022-10-05 11:42:36 +0200

    Clase Promedios

---
+++ Platzi/2- Taller de Formulas matemáticas en JavaScript/6. Clase Mediana/platzimath.js
@@ -0,0 +1,39 @@
+function esPar(lista) {
+  return !(lista.length % 2);
+}
+function esImpar(lista) {
+  return lista.length % 2;
+}
+
+function calcularMediana(lista) {
+  const listaEsPar = esPar(lista);
+
+  if (listaEsPar) {
+    // ...
+  } else {
+    const indexMitadListaImpar = Math.floor(lista.length / 2);
+    const medianaListaImpar = lista[indexMitadListaImpar];
+    console.log(indexMitadListaImpar);
+    console.log(medianaListaImpar);
+    return medianaListaImpar;
+  }
+}
+// [1,2,3,4]
+function calcularPromedio(lista) {
+  // let sumaLista = 0;
+  // for (let i = 0; i < lista.length; i++) {
+  //   sumaLista = sumaLista + lista[i];
+  // }
+
+  function sumarTodosElementos(valorAcumulado, nuevoValor) {
+    return valorAcumulado + nuevoValor;
+  }
+  // const ejemplo = (a, b) => a + b;
+  // const sumarTodosElementos = (valorAcumulado, nuevoValor) => valorAcumulado + nuevoValor;
+  // const sumaLista = lista.reduce((a, b) => a + b);
+
+  const sumaLista = lista.reduce(sumarTodosElementos);
+  const promedio = sumaLista / lista.length;
+  console.log(promedio);
+  return promedio;
+}
